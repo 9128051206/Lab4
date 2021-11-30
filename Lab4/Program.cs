9ec3970dafe910a8c45ddad637cacbfcddbb2a3c
91280 <91280@HP-ONQCSD8>
2021-11-30 13:42:46 +0500

    Добавьте файлы проекта.

---
+++ Lab4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число s>0");
+            int s=Convert.ToInt32(Console.ReadLine());
+            for(int n=1, sum=0; n<=s; n++)
+            {
+                Console.WriteLine("Квадрат числа {0} равен {1}", n, sum+=n*2-1);
+                Console.ReadKey();
+            }
+           
+        }   
+    }
+}
